403472b71d0c41366253e5cc1ae49674e32dd7b7
vbuilvicente <vbuilvicente@gmail.com>
2016-04-04 12:19:56 -0400

    update

--- app/controllers/QueueManager.js
+++ app/controllers/QueueManager.js
@@ -75,7 +75,7 @@ exports.updateQueue = function (req, res) {
                  res.json({message:err.code});
             }
             else{
-                res.json({message:  queue.tls});
+                res.json({message: "request " +req.body.tls});
             }
 
         });
