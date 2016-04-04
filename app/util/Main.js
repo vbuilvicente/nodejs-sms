91fb904171313eccf160bbe00359804e3e897a85
vbuilvicente <vbuilvicente@gmail.com>
2016-04-04 11:39:46 -0400

    update

--- app/util/Main.js
+++ app/util/Main.js
@@ -14,19 +14,13 @@ function init() {
         for (i in queue) {
 
             var mailListener = new MailListener({
-                // username: queue[i].username,
-                // password: queue[i].password,
-                // host: queue[i].host,
-                // port: queue[i].port,
-                // tls: queue[i].tls,
-                // tlsOptions: {rejectUnauthorized: false},
+                username: queue[i].username,
+                password: queue[i].password,
+                host: queue[i].host,
+                port: queue[i].port,
+                tls: queue[i].tls,
+                tlsOptions: {rejectUnauthorized: false},
                 mailbox: "INBOX",
-                xoauth2:{
-                    user: "vbuilvicente@gmail.com",
-                    clientId: "379811545006-1kipufg5ah7vk7352d8duq8vfin4ktjf.apps.googleusercontent.com",
-                    clientSecret: "Ftk6uHYOh_503R44n_hIBPTH",
-                    timeout: 3600
-                },
                 markSeen: true,
                 fetchUnreadOnStart: true,
                 attachments: false,
