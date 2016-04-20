97a4422e5ec46e6b278285c9d3ad68a3fd63b533
vbuilvicente <vbuilvicente@gmail.com>
2016-04-19 22:58:52 -0400

    Agregando log

--- app/util/SmsManager.js
+++ app/util/SmsManager.js
@@ -1,4 +1,4 @@
-var Client = require('node-rest-client').Client;
+
 var rest = require('restler');
 var nconf = require('nconf');
 var client = new Client();
@@ -15,25 +15,12 @@ exports.send = function (number, text) {
         senderid: "SMS",
         message: value
     };
-    // var args = {
-    //     data: data,
-    //     headers: { "Content-Type": "application/json" }
-    //
-    // };
-    //
-    // client.registerMethod("sendSMS", "http://old.cubalan.com/sendsms.php", "POST");
-    // client.methods.sendSMS(args, function (data, response) {
-    //     // parsed response body as js object
-    //     console.log("data",data);
-    //     // raw response
-    //     console.log("response",response);
-    //
-    // });
+    
     rest.post('http://old.cubalan.com/sendsms.php', {
         data: data,
     }).on('complete', function(data, response) {
         console.log("data",data);
-        console.log("response",response)
+       
     });
 
 }
